65c7c16a4b15e59f496669e910cd906b95a02afc
Eugene Dorfman <eugene.dorfman@gmail.com>
2022-03-15 12:55:48 +0200

    update script

---
+++ update.sh
@@ -0,0 +1,5 @@
+py3.9 #alias
+python tg_channel_download.py
+python tg_channel_upload_media.py
+echo "\copy (select * from messages where chat_id=1722167948 order by send_date) to PravdaGerashchenko.csv delimiter ',' csv header;" | psql -U tgclient tghistory 
+python upload.py PravdaGerashchenko.csv
