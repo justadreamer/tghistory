c7409beeecbb5ccd37c244eb63e973402b67dabf
Eugene Dorfman <eugene.dorfman@gmail.com>
2022-03-15 23:11:38 +0200

    multiple chats

--- tg_channel_download.py
+++ tg_channel_download.py
@@ -18,117 +18,127 @@ from datetime import timezone
 from datetime import timedelta
 from yaml import Loader
 
-#load config:
-with open('config.yml','r') as f:
-    config = yaml.load(f, Loader)['default']
-    print(config)
+debug = True
 
-# global service objects:
-connection_string = "dbname={} user={} password={} host={} port={}".format(config['dbname'],
-                                                               config['dbuser'],
-                                                               config['dbpassword'],
-                                                               config['dbhost'],
-                                                               config['dbport'])
-db = DB(connection_string=connection_string)
+class Downloader:
+    def __init__(self, db, client):
+        self.db = db
+        self.client = client
 
-client = TelegramClient(config['user_phone'], config['app_id'], config['api_hash'])
-client.start()
+    async def download_history_batch(self, channel, offset_id):
+        history = await self.client(GetHistoryRequest(
+            peer=channel,
+            offset_id=offset_id,
+            offset_date=None,
+            add_offset=0,
+            limit=100 if not debug else 10,
+            max_id=0,
+            min_id=0,
+            hash=0
+        ))
+        min_date = datetime.now().replace(tzinfo=timezone(offset=timedelta()))
+        offset_id = 0
 
-pp = pprint.PrettyPrinter(indent=2)
+        messages = history.messages
 
-async def download_history_batch(channel, offset_id):
-    history = await client(GetHistoryRequest(
-        peer=channel,
-        offset_id=offset_id,
-        offset_date=None,
-        add_offset=0,
-        limit=100,
-        max_id=0,
-        min_id=0,
-        hash=0
-    ))
-    min_date = datetime.now().replace(tzinfo=timezone(offset=timedelta()))
-    offset_id = 0
+        for message in messages:
+            print(message)
+            message_dict = dict()
+            offset_id = message.id
+            date = message.date
+            if date < min_date:
+                min_date = date
 
-    messages = history.messages
+            media = message.media
+            filename = None  #extract either filename or url as metadata to store with the message record in the DB
+            url = None
+            content_type = 'text'
+            if isinstance(media, MessageMediaDocument):
+                attributes: [DocumentAttributeFilename] = message.media.document.attributes
 
-    for message in messages:
-        print(message)
-        message_dict = dict()
-        offset_id = message.id
-        date = message.date
-        if date < min_date:
-            min_date = date
-
-        media = message.media
-        filename = None  #extract either filename or url as metadata to store with the message record in the DB
-        url = None
-        content_type = 'text'
-        if isinstance(media, MessageMediaDocument):
-            attributes: [DocumentAttributeFilename] = message.media.document.attributes
-
-            for attribute in attributes:
-                if isinstance(attribute, DocumentAttributeFilename):
-                    filename = attribute.file_name
-                    break
-                elif isinstance(attribute, DocumentAttributeVideo):
-                    filename = f"{message.id}.mp4"
-                    content_type = 'video'
-        elif isinstance(media, MessageMediaPhoto):
-            filename = f"{message.id}.jpeg"
-            content_type = 'photo'
-        elif isinstance(media, MessageMediaWebPage):
-            if isinstance(media.webpage, WebPage):
-                url = print(media.webpage.url)
-            content_type = 'web'
+                for attribute in attributes:
+                    if isinstance(attribute, DocumentAttributeFilename):
+                        filename = attribute.file_name
+                        break
+                    elif isinstance(attribute, DocumentAttributeVideo):
+                        filename = f"{message.id}.mp4"
+                        content_type = 'video'
+            elif isinstance(media, MessageMediaPhoto):
+                filename = f"{message.id}.jpeg"
+                content_type = 'photo'
+            elif isinstance(media, MessageMediaWebPage):
+                if isinstance(media.webpage, WebPage):
+                    url = print(media.webpage.url)
+                content_type = 'web'
 
 
-        # download media
-        metadata = None
-        if filename is not None:
-            filepath = f'download/{channel.id}/{filename}'
-            if not os.path.exists(filepath):
-                print(f"downloading {filepath}")
-                await client.download_media(message, filepath)
-            else:
-                print(f"{filepath} already downloaded")
-            metadata = filename
-        elif url is not None:
-            metadata = url
-        #save message into DB:
-        db.store_message({ 'id': message.id,
-                           'chat_id': channel.id,
-                           'date': message.date,
-                           'sender_user_id': channel.id,
-                           'content': {
-                               'text': message.message,
-                               '@type': content_type
-                           },
-                           'metadata': metadata
-                           })
+            # download media
+            metadata = None
+            if filename is not None:
+                filepath = f'download/{channel.id}/{filename}'
+                if not os.path.exists(filepath):
+                    print(f"downloading {filepath}")
+                    await self.client.download_media(message, filepath)
+                else:
+                    print(f"{filepath} already downloaded")
+                metadata = filename
+            elif url is not None:
+                metadata = url
+            #save message into DB:
+            self.db.store_message({ 'id': message.id,
+                               'chat_id': channel.id,
+                               'date': message.date,
+                               'sender_user_id': channel.id,
+                               'content': {
+                                   'text': message.message,
+                                   '@type': content_type
+                               },
+                               'metadata': metadata
+                               })
 
-    return min_date, offset_id
+        return min_date, offset_id
 
-async def main(chat_name):
-    print(chat_name)
-    dialogs = await client.get_dialogs()
-    dialog = list(filter(lambda dialog: dialog.name == chat_name, dialogs))[0]
-    channel = dialog.entity
-    db.store_chat( { 'id':channel.id, 'title':channel.title } )
+    async def download_chat(self, chat_name, redownload = False):
+        print(chat_name)
+        await self.client.start()
+        dialogs = await self.client.get_dialogs()
+        dialog = list(filter(lambda dialog: dialog.name == chat_name, dialogs))[0]
+        channel = dialog.entity
+        self.db.store_chat( { 'id':channel.id, 'title':channel.title } )
 
-    if config['redownload']:
-        lower_bound_date = None
-    else:
-        lower_bound_date = db.get_last_stored_message_date(channel.id)
+        if redownload:
+            lower_bound_date = None
+        else:
+            lower_bound_date = self.db.get_last_stored_message_date(channel.id)
 
-    if lower_bound_date is None:
-        lower_bound_date = datetime.fromisoformat("2022-02-24 00:00:00").replace(tzinfo=timezone(offset=timedelta()))
+        if lower_bound_date is None:
+            lower_bound_date = datetime.fromisoformat("2022-02-24 00:00:00").replace(tzinfo=timezone(offset=timedelta()))
 
-    date = datetime.now().replace(tzinfo=timezone(offset=timedelta()))
-    offset_id = 0
-    while date > lower_bound_date:
-        date, offset_id = await download_history_batch(channel, offset_id)
-        print(date, offset_id)
+        date = datetime.now().replace(tzinfo=timezone(offset=timedelta()))
+        offset_id = 0
+        while date > lower_bound_date:
+            date, offset_id = await self.download_history_batch(channel, offset_id)
+            print(date, offset_id)
+            if debug:
+                break
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main(config['chats'][0]))
+async def main():
+    # load config:
+    with open('config.yml', 'r') as f:
+        config = yaml.load(f, Loader)['default']
+        print(config)
+
+    connection_string = "dbname={} user={} password={} host={} port={}".format(config['dbname'],
+                                                                               config['dbuser'],
+                                                                               config['dbpassword'],
+                                                                               config['dbhost'],
+                                                                               config['dbport'])
+    db = DB(connection_string=connection_string)
+    client = TelegramClient(config['user_phone'], config['app_id'], config['api_hash'])
+
+    downloader = Downloader(db, client)
+    for chat in config['chats']:
+        await downloader.download_chat(chat, redownload = config['redownload'])
+
+asyncio.run(main())
+
--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@ py3.9 #alias
 python tg_channel_download.py
 python tg_channel_upload_media.py
 echo "\copy (select * from messages where chat_id=1722167948 order by send_date) to PravdaGerashchenko.csv delimiter ',' csv header;" | psql -U tgclient tghistory 
-python upload.py PravdaGerashchenko.csv
+python upload.py PravdaGerashchenko.csv tghistory
