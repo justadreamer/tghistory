20b201cdce7b83ba9e8e2f90fc65e226788355f2
Eugene Dorfman <eugene.dorfman@gmail.com>
2022-03-16 17:46:25 +0200

    upload: checking message_id

--- tg_channel_upload_media.py
+++ tg_channel_upload_media.py
@@ -22,18 +22,18 @@ download_dir = config['download'] if 'download' in config else 'download'
 
 def upload():
     for subdir in os.listdir(download_dir):
-        channel_id = subdir
+        channel_id = int(subdir)
         folder = Folder(PurePath(f'tghistory/{channel_id}'), createIfNotExists=True)
         messages = db.get_messages(chat_id=channel_id)
-
-        for message in messages:
-            print(message)
-            message_id = message[0]
-            filename = message[len(message)-2]
-            if filename is not None:
-                filepath = os.path.join(download_dir, subdir, filename)
-                if os.path.exists(filepath):
-                    gfile = folder.upload(filepath)
-                    db.update_message_upload(message_id, gfile['alternateLink'])
+        if messages is not None:
+            for message in messages:
+                print(message)
+                message_id = message[0]
+                filename = message[len(message)-2]
+                if filename is not None:
+                    filepath = os.path.join(download_dir, subdir, filename)
+                    if os.path.exists(filepath):
+                        gfile = folder.upload(filepath)
+                        db.update_message_upload(message_id, gfile['alternateLink'])
 
 upload()
