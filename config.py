c1b29b4a6e50c19e829978cc6cb576fbc1b9f9cd
Eugene Dorfman <eugene.dorfman@gmail.com>
2022-03-17 12:34:34 +0200

    config.py module

---
+++ config.py
@@ -0,0 +1,19 @@
+from postgres_connection import DB
+import yaml
+from yaml import Loader
+
+def get_config():
+    with open('config.yml', 'r') as f:
+        config = yaml.load(f, Loader)['default']
+        print(config)
+    return config
+
+def get_db():
+    config = get_config()
+    connection_string = "dbname={} user={} password={} host={} port={}".format(config['dbname'],
+                                                                               config['dbuser'],
+                                                                               config['dbpassword'],
+                                                                               config['dbhost'],
+                                                                               config['dbport'])
+    db = DB(connection_string=connection_string)
+    return db
